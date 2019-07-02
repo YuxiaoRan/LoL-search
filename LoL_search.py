0e0a524c9546a33b258e544b149afcdda088f276
Sean Ran <sean.ran@catapultsystems.com>
2019-07-02 11:36:15 -0500

    initial commit

---
+++ LoL_search.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import sys
+
+# parse username
+username = ""
+for i in range(1, len(sys.argv)):
+	username += sys.argv[i]
+	username += " "
+username = username[0 : len(username) - 1]
+
+url = "https://na.op.gg/"
+
+driver = webdriver.Chrome(r"C:\Users\sran\Downloads\chromedriver.exe")
+
+driver.get(url)
+
+# search for user
+driver.find_element_by_name("userName").send_keys(username)
+driver.find_element_by_class_name("summoner-search-form__button").click()
+
+driver.find_elements_by_css_selector("a.Button")[-1].click()
+
+time.sleep(10)
+
+driver.quit()
